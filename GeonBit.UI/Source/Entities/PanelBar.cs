abd3457a055f319e5668d875542b27cce9ed9c23
Marcel Härtel <m.haertel@gmx.net>
2021-08-26 18:40:14 +0200

    WIP: Added a basic PanelBar to make it easy to create a Panel with Entities in it.

---
+++ GeonBit.UI/Source/Entities/PanelBar.cs
@@ -0,0 +1,48 @@
+﻿using GeonBit.UI.Entities;
+using GeonBit.UI.Source.Input;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeonBit.UI.Source.Entities
+{
+    /// <summary>
+    /// A PanelBar makes it easy to create a Panel with Entities in it.
+    /// </summary>
+    [System.Serializable]
+    public class PanelBar : PanelGamePad
+    {
+        /// <summary>
+        /// Static ctor.
+        /// </summary>
+        static PanelBar()
+        {
+            Entity.MakeSerializable(typeof(PanelBar));
+        }
+
+        /// <summary>
+        /// Creates a basic PanelBar with a custom amount of entities.
+        /// </summary>
+        public PanelBar(params Entity[] entities)
+            : base(new Vector2(0, 0))
+        {
+            int entitiesCount = entities.Length;
+
+            Padding = new Vector2(8);
+
+            for (int i = 0; i < entitiesCount; i++)
+            {
+                entities[i].Size = new Vector2(1f / entitiesCount, 0);
+                entities[i].Anchor = Anchor.AutoInline;
+                entities[i].Identifier = GetIdentifier(HierarchyIdentifier.PanelContent);
+                AddChild(entities[i]);
+            }
+
+            Identifier = GetIdentifier(HierarchyIdentifier.PanelGrid);
+        }
+    }
+}
