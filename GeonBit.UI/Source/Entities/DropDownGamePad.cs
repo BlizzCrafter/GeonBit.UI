c2f4c6da4eabc70ef8623aa791a1711627404102
Marcel Härtel <m.haertel@gmx.net>
2021-09-07 15:46:21 +0200

    Added DropDownGamePad and DropDownPanel both with GamePad support to the project.

---
+++ GeonBit.UI/Source/Entities/DropDownGamePad.cs
@@ -0,0 +1,185 @@
+﻿using GeonBit.UI.Entities;
+using GeonBit.UI.Source.Input;
+using Microsoft.Xna.Framework;
+
+namespace GeonBit.UI.Source.Entities
+{
+    /// <summary>
+    /// DropDown is just like a list, but it only shows the currently selected value unless clicked on (the list is
+    /// only revealed while interacted with).
+    /// </summary>
+    [System.Serializable]
+    public class DropDownGamePad : DropDown, IEntityGamePad
+    {
+        /// <summary>
+        /// Static ctor.
+        /// </summary>
+        static DropDownGamePad()
+        {
+            Entity.MakeSerializable(typeof(DropDownGamePad));
+        }
+
+        #region Events
+
+        /// <summary>
+        /// Calls the DoOnClick() event internally.
+        /// </summary>
+        public void TriggerOnButtonClick()
+        {
+            DoOnClick();
+
+            if (!_Selected)
+            {
+                _Selected = true;
+                ArrowDownImage.Visible = true;
+                SetSelectedDropDownInParent(this); //Select this DropDown.
+            }
+            else
+            {
+                _Selected = false;
+                ArrowDownImage.Visible = false;
+                SetSelectedDropDownInParent(null); //Deselect this DropDown.
+            }
+
+            //Initial selection. This is happening automatically without user input.
+            if (!HasSelectedValue) SelectNextItem();
+        }
+        /// <summary>
+        /// Calls the DoOnMouseEnter() event internally.
+        /// </summary>
+        public void TriggerOnSelect()
+        {
+            DoOnMouseEnter();
+
+            SelectedTextPanel.FillColor = GamePadSetup.SelectedColor;
+        }
+        /// <summary>
+        /// Calls the DoOnMouseLeave() event internally.
+        /// </summary>
+        public void TriggerOnDeSelect()
+        {
+            DoOnMouseLeave();
+
+            SelectedTextPanel.FillColor = GamePadSetup.DefaultColor;
+            ArrowDownImage.Visible = false;
+
+            SetSelectedDropDownInParent(null);
+        }
+        /// <summary>
+        /// Calls the OnMouseDown() event internally.
+        /// </summary>
+        public void TriggerOnButtonDown()
+        {
+            DoOnMouseDown();
+        }
+        /// <summary>
+        /// Calls the DoWhileMouseDown() event internally.
+        /// </summary>
+        public void TriggerDoWhileButtonDown()
+        {
+            DoWhileMouseDown();
+        }
+        /// <summary>
+        /// Calls the DoWhileMouseHover() event internally.
+        /// </summary>
+        public void TriggerDoWhileHover()
+        {
+            DoWhileMouseHover();
+        }
+        /// <summary>
+        /// Calls the DoOnMouseReleased() event internally.
+        /// </summary>
+        public void TriggerOnButtonReleased()
+        {
+            DoOnMouseReleased();
+        }
+        /// <summary>
+        /// Calls the DoOnValueChange() event internally.
+        /// </summary>
+        public void TriggerOnValueChanged()
+        {
+            DoOnValueChange();
+        }
+
+        #endregion Events
+
+        private bool _Selected;
+
+        /// <summary>
+        /// Select the next item in the SelectList.
+        /// </summary>
+        public void SelectNextItem()
+        {
+            int selection = SelectedIndex;
+            selection = NextItemCheck(selection);
+
+            while (SelectList.LockedItems.ContainsKey(selection) && SelectList.LockedItems[selection])
+            {
+                selection = NextItemCheck(selection);
+            }
+
+            SelectedIndex = selection;
+            ScrollToSelected();
+        }
+        private int NextItemCheck(int selection)
+        {
+            selection++;
+            return MaxCountCheck(selection);
+        }
+        private int MaxCountCheck(int value)
+        {
+            if (value > Count - 1) return 0;
+            else return value;
+        }
+
+        /// <summary>
+        /// Select the previous item in the SelectList.
+        /// </summary>
+        public void SelectPreviousItem()
+        {
+            int selection = SelectedIndex;
+            selection = PreviousItemCheck(selection);
+
+            while (SelectList.LockedItems.ContainsKey(selection) && SelectList.LockedItems[selection])
+            {
+                selection = PreviousItemCheck(selection);
+            }
+
+            SelectedIndex = selection;
+            ScrollToSelected();
+        }
+        private int PreviousItemCheck(int selection)
+        {
+            selection--;
+            return MinCountCheck(selection);
+        }
+        private int MinCountCheck(int value)
+        {
+            if (value < 0) return Count - 1;
+            else return value;
+        }
+
+        private void SetSelectedDropDownInParent(DropDownGamePad dropDownGamePad)
+        {
+            if (Parent is DropDownPanel)
+            {
+                ((DropDownPanel)Parent).SelectedDropDown = dropDownGamePad;
+            }
+        }
+
+        /// <summary>
+        /// Create the DropDown list with gamepad support.
+        /// </summary>
+        /// <param name="items">The items you want to add to the DropDown.</param>
+        public DropDownGamePad(params string[] items) :
+            base(Vector2.Zero, Anchor.Auto, Vector2.Zero, PanelSkin.ListBackground, null, false)
+        {
+            Identifier = PanelGamePad.GetIdentifier(HierarchyIdentifier.PanelContent);
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                AddItem(items[i]);
+            }
+        }
+    }
+}
---
+++ GeonBit.UI/Source/Entities/DropDownPanel.cs
@@ -0,0 +1,73 @@
+﻿using GeonBit.UI.Entities;
+using Microsoft.Xna.Framework;
+
+namespace GeonBit.UI.Source.Entities
+{
+    /// <summary>
+    /// This PanelGamePad contains a DropDown with gamepad support.
+    /// </summary>
+    public class DropDownPanel : PanelGamePad
+    {
+        /// <summary>
+        /// The currently selected drop down reference.
+        /// </summary>
+        public DropDownGamePad SelectedDropDown { get; internal set; }
+
+        /// <summary>
+        /// Creates the GamePad-Panel with the default skin and based on a regular Panel-Entity.
+        /// </summary>
+        /// <param name="size">Panel size.</param>
+        /// <param name="anchor">Position anchor.</param>
+        /// <param name="offset">Offset from anchor position.</param>
+        /// <param name="entities">The items you want to add to the SelectList.</param>
+        public DropDownPanel(
+            Vector2 size,
+            Anchor anchor = Anchor.Center,
+            Vector2? offset = null,
+            params Entity[] entities)
+            : base(size, anchor, offset)
+        {
+            Padding = new Vector2(6);
+
+            for (int i = 0; i < entities.Length; i++)
+            {
+                if (string.IsNullOrEmpty(entities[i].Identifier))
+                {
+                    entities[i].Identifier = GetIdentifier(HierarchyIdentifier.PanelContent);
+                }
+
+                entities[i].Size = new Vector2(0, 1f / entities.Length);
+                entities[i].Padding = Vector2.Zero;
+                entities[i].SpaceAfter = Vector2.Zero;
+                entities[i].SpaceBefore = Vector2.Zero;
+
+                AddChild(entities[i]);
+            }
+
+            InvisiblePanel = true;
+        }
+
+        /// <summary>
+        /// Select an item from the DropDown by using the SelectedIndex.
+        /// </summary>
+        protected override void SelectContent(PanelDirection direction)
+        {
+            if (direction == PanelDirection.Down)
+            {
+                if (SelectedDropDown != null)
+                {
+                    SelectedDropDown.SelectNextItem();
+                }
+                else base.SelectContent(direction);
+            }
+            else if (direction == PanelDirection.Up)
+            {
+                if (SelectedDropDown != null)
+                {
+                    SelectedDropDown.SelectPreviousItem();
+                }
+                else base.SelectContent(direction);
+            }
+        }
+    }
+}
