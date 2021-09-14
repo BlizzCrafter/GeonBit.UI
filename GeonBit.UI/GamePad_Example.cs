79f50748ea5ed93c8002f2ad491fc859ef2aacf8
Marcel Härtel <m.haertel@gmx.net>
2021-09-14 10:03:41 +0200

    Added additional comments and made the sample project more clear by doing some refactorings.

--- GeonBit.UI/GamePad_Example.cs
+++ GeonBit.UI/GamePad_Example.cs
@@ -11,7 +11,7 @@ using static GeonBit.UI.Utils.MessageBox;
 namespace GeonBit.UI
 {
     /// <summary>
-    /// 
+    /// This is the main 'Game' instance for your game.
     /// </summary>
     public class GamePad_Example : Game
     {
@@ -22,7 +22,7 @@ namespace GeonBit.UI
         MessageBoxGamePad _MessageBox;
 
         List<PanelGamePad> _Examples = new List<PanelGamePad>();
-        int currentExample = -1;
+        int _CurrentExample = -1;
 
         /// <summary>
         /// Create the game instance.
@@ -56,13 +56,19 @@ namespace GeonBit.UI
 
             UserInterface.Active.GlobalScale = 0.5f;
 
+            //IMPORTANT:
+            //Call this line to initialize basic GamePad-Supported functionalities
+            //and get access to specialized RichParagraphStyleInstructions with usual GamePad-Colors.
             GamePadSetup.Initialize();
 
-            InitGamePadExample();
+            //Initialize the UI-Scenarios.
+            InitializeGamePadExample();
         }
 
-        private void InitGamePadExample()
+        private void InitializeGamePadExample()
         {
+            //The flat selection sample shows how to create a simple GamePad-Supported (Main-)Menu.
+            //Deep selection hirarchy is often not needed in such a scenario.
             #region FLAT SELECTION SAMPLE
 
             _FlatRootGridPanel = new PanelGrid(
@@ -90,7 +96,7 @@ namespace GeonBit.UI
                     new SliderGamePad(0, 20, Vector2.Zero),
                     new Paragraph("Enemy-Count") { Identifier = PanelGamePad.GetIdentifier(HierarchyIdentifier.None) },
                     new SliderGamePad(0, 200, Vector2.Zero),
-                    new ButtonGamePad("Reset to Default", skin: ButtonSkin.Alternative, anchor: Anchor.AutoInline)
+                    new ButtonGamePad("Message-Box-Test", skin: ButtonSkin.Alternative, anchor: Anchor.AutoInline) { OnClick = SayHelloWorldClicked }
                     )
                     { Name = "Options" }
                     ));
@@ -102,10 +108,14 @@ namespace GeonBit.UI
                         { AlignToCenter = true, WrapWords = false, BreakWordsIfMust = false, AddHyphenWhenBreakWord = false }));
             _FlatRootGridPanel.GetGridPanel(Anchor.BottomCenter).Size = new Vector2(0, 0.1f);
 
+            //ATTENTION:
+            //Normally you would start the panel selection here, but if you have multiple UI's you should do it like this sample project.
             //_FlatRootGridPanel.StartPanelSelection();
 
             #endregion FLAT SELECTION SAMPLE
 
+            //The deep selection sample shows how to create a complex GamePad-Supported Multi-Mix-Menu.
+            //Flat selection hirarchy can't do that, because it wouldn't be possible to select nested panels then.
             #region DEEP SELECTION SAMPLE
 
             _DeepRootGridPanel = new PanelGrid(
@@ -126,7 +136,7 @@ namespace GeonBit.UI
                         panelGrid.Children[i].AddChild(new Header($"Panel - #{i}"));
 
                         // add some buttons
-                        panelGrid.Children[i].AddChild(new ButtonGamePad("Hello World") { ToolTipText = $"This is Test-Tooltip #{i}", OnClick = SayHelloWorldClicked });
+                        panelGrid.Children[i].AddChild(new ButtonGamePad("Hover Me") { ToolTipText = $"This is Test-Tooltip #{i}", OnClick = SayHelloWorldClicked });
                         panelGrid.Children[i].AddChild(new ButtonGamePad("Toggle") { ToggleMode = true });
                     }
                 }
@@ -175,10 +185,10 @@ namespace GeonBit.UI
 
             _DeepRootGridPanel.GetGridPanel(Anchor.TopCenter).AddChild(
                 new PanelBar(Orientation.Horizontal,
-                    new ButtonGamePad("Button-1", anchor: Anchor.AutoInline) { ToolTipText = "This is Test-Tooltip #1" },
+                    new ButtonGamePad("Button-1", anchor: Anchor.AutoInline),
                     new ButtonGamePad("Non-Selectable", HierarchyIdentifier.None, anchor: Anchor.AutoInline) { Enabled = false },
-                    new ButtonGamePad("Button-3", anchor: Anchor.AutoInline) { ToolTipText = "This is Test-Tooltip #3" },
-                    new ButtonGamePad("Button-4", anchor: Anchor.AutoInline) { ToolTipText = "This is Test-Tooltip #4" },
+                    new ButtonGamePad("Button-2", anchor: Anchor.AutoInline),
+                    new ButtonGamePad("Hover Me", anchor: Anchor.AutoInline) { ToolTipText = "This is a Test-Tooltip" },
                     new ButtonGamePad("Toggle", skin: ButtonSkin.Alternative, anchor: Anchor.AutoInline) { ToggleMode = true }
                     ));
 
@@ -218,6 +228,8 @@ namespace GeonBit.UI
                     );
             _DeepRootGridPanel.GetGridPanel(Anchor.BottomCenter).Size = new Vector2(0, 0.1f);
 
+            //ATTENTION:
+            //Normally you would start the panel selection here, but if you have multiple UI's you should do it like this sample project.
             //_DeepRootGridPanel.StartPanelSelection();
 
             //The CenterRight GridPanel is not visible by default (panelGrid with index 0 is selected first).
@@ -241,9 +253,54 @@ namespace GeonBit.UI
 
             #endregion DEEP SELECTION SAMPLE
 
+            //Start the first UI-Scenario
             NextExample();
 
             base.Initialize();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (!IsActive)
+                return;
+
+            // exit on escape
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+
+            UserInterface.Active.Update(gameTime);
+            if (_MessageBox != null) _MessageBox.Update(gameTime);
+
+            if (UserInterface.Active.GamePadInputProvider.GamePadButtonClick(Buttons.Y))
+            {
+                //When switching UI's you should make sure that you are currently in the "Outer-Selection"-Mode.
+                //This is to avoid selection issues when shifting to multiple UI-Scenarios.
+                if (PanelGamePad.RootGrid.IsOuterSelection)
+                {
+                    NextExample();
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            UserInterface.Active.Draw(spriteBatch);
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            UserInterface.Active.DrawMainRenderTarget(spriteBatch);
+
+            base.Draw(gameTime);
         }
 
         private void SayHelloWorldClicked(Entity entity)
@@ -274,64 +331,21 @@ namespace GeonBit.UI
         }
 
         /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Update(GameTime gameTime)
-        {
-            if (!IsActive)
-                return;
-
-            // exit on escape
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
-
-            UserInterface.Active.Update(gameTime);
-            if (_MessageBox != null) _MessageBox.Update(gameTime);
-
-            if (UserInterface.Active.GamePadInputProvider.GamePadButtonClick(Buttons.Y))
-            {
-                if (PanelGamePad.RootGrid.IsOuterSelection)
-                {
-                    NextExample();
-                }
-            }
-
-            base.Update(gameTime);
-        }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
-        protected override void Draw(GameTime gameTime)
-        {
-            UserInterface.Active.Draw(spriteBatch);
-
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            UserInterface.Active.DrawMainRenderTarget(spriteBatch);
-
-            base.Draw(gameTime);
-        }
-
-        /// <summary>
         /// Show next UI example.
         /// </summary>
         public void NextExample()
         {
-            currentExample++;
+            _CurrentExample++;
             UpdateAfterExampleChange();
         }
 
         /// <summary>
         /// Called after we change current example index, to hide all examples
-        /// except for the currently active example + disable prev / next buttons if
-        /// needed (if first or last example).
+        /// except for the currently active example.
         /// </summary>
         protected void UpdateAfterExampleChange()
         {
-            if (currentExample > 1) currentExample = 0;
+            if (_CurrentExample > 1) _CurrentExample = 0;
 
             // hide all panels and show current example panel
             foreach (PanelGamePad panel in _Examples)
@@ -339,7 +353,10 @@ namespace GeonBit.UI
                 panel.Visible = false;
             }
 
-            PanelGamePad.RootGrid = _Examples[currentExample];
+            //Set new RootGrid.
+            PanelGamePad.RootGrid = _Examples[_CurrentExample];
+
+            //Start panel selection.
             PanelGamePad.RootGrid.StartPanelSelection();
         }
     }
